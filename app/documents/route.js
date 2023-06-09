fd946952a2ffe991daf0e8c7ea94345ca10ff5c6
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2023-06-09 23:50:21 +0200

    Refactor backend module

--- app/documents/route.js
+++ app/documents/route.js
@@ -1,7 +1,9 @@
-import ExcelDocumentRepository from "../../backend/lib/infrastructure/documents/ExcelDocumentRepository"
+
 
 import { NextResponse } from 'next/server'
+import ExcelDocumentRepository from "total-budget-tracker-backend"
  
+console.log('ExcelDocumentRepository', ExcelDocumentRepository)
 export async function GET() {
   const repository = new ExcelDocumentRepository()
   const path = `${process.cwd()}/temp/example.xls`
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,2 @@
 import ExcelDocumentRepository from "./infrastructure/documents/ExcelDocumentRepository.js"
-
-const expenses = new ExcelDocumentRepository().read('../../data/temp/example.xls')
-
-console.log(JSON.stringify(expenses))
+export default ExcelDocumentRepository
