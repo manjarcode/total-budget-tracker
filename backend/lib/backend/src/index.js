addc3b8115c09d0bfed99439e123aee4b16b2436
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2023-06-09 23:23:18 +0200

    Remove lib folder

--- backend/lib/backend/src/index.js
+++
@@ -1,3 +0,0 @@
-import ExcelDocumentRepository from "./infrastructure/documents/ExcelDocumentRepository.js";
-const expenses = new ExcelDocumentRepository().read('../../data/temp/example.xls');
-console.log(JSON.stringify(expenses));
