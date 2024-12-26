2ec57b2fba3610348c2985034d7ea194625d943b
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2024-12-26 12:18:02 +0100

    Improve layout

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,24 @@
 import {CacheProvider} from '@emotion/react'
-import {CssBaseline, ThemeProvider} from '@mui/material'
+import {Box, CssBaseline, ThemeProvider} from '@mui/material'
 
 import createEmotionCache from './emotionCache.js'
 import {theme} from './theme.js'
+import style from './layout.module.css'
 
 const clientSideEmotionCache = createEmotionCache()
 
 export default function RootLayout(props) {
   const {children, emotionCache = clientSideEmotionCache} = props
   return (
-    <html lang="en">
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
-          <body>
-            <CssBaseline />
-            {children}
-          </body>
+          <CssBaseline />
+          <Box className={style.container}>
+            <Box className={style.main}>
+              {children}
+            </Box>
+          </Box>
         </ThemeProvider>
       </CacheProvider>
-    </html>
   )
 }
