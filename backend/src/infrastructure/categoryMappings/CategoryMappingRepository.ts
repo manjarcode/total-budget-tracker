1c46920e4b2379777fa854b847b02e0349d66ab4
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2025-04-27 13:43:49 +0200

    Fix wrong default mapping

--- backend/src/infrastructure/categoryMappings/CategoryMappingRepository.ts
+++ backend/src/infrastructure/categoryMappings/CategoryMappingRepository.ts
@@ -10,12 +10,12 @@ export default class CategoryMappingRepository {
 
   async list() : Promise<CategoryMappingCollection> {
     const categories =  [
-      new CategoryMapping('Lidl pavones', 'cbe51e91-c158-44af-8fa6-1bc978c34eec', 'Supermercado'),
-      new CategoryMapping('Market ccaminos', 'cbe51e91-c158-44af-8fa6-1bc978c34eec', 'Supermercado'),
-      new CategoryMapping('Adeudo hacienda de pavones 292 com prop', '20e08d21-305a-4890-8d29-3caf3672806c', 'Comunidad'),
-      new CategoryMapping('Adeudo naturgy clientes, s.a.u.', '20e08d21-305a-4890-8d29-3caf3672806c', 'Electricidad'),
-      new CategoryMapping('Cargo por amortizacion de prestamo/credito', '20e08d21-305a-4890-8d29-3caf3672806c', 'Alquiler / Hipoteca'),
-      new CategoryMapping('Adeudo digi spain telecom sl', '20e08d21-305a-4890-8d29-3caf3672806c', 'Internet'),
+      new CategoryMapping('Lidl pavones', 'Alimentación', 'Supermercado'),
+      new CategoryMapping('Market ccaminos', 'Alimentación', 'Supermercado'),
+      new CategoryMapping('Adeudo hacienda de pavones 292 com prop', 'Vivienda', 'Comunidad'),
+      new CategoryMapping('Adeudo naturgy clientes, s.a.u.', 'Vivienda', 'Electricidad'),
+      new CategoryMapping('Cargo por amortizacion de prestamo/credito', 'Vivienda', 'Alquiler / Hipoteca'),
+      new CategoryMapping('Adeudo digi spain telecom sl', 'Vivienda', 'Internet'),
     ]
 
     return new CategoryMappingCollection(categories)
